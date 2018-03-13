4874667f865927b482c69feccd1819145f409168
llc <touldeguiacranmer@gmail.com>
2018-03-13 04:30:03 -0500

    implemented create Node with diagrams

--- linkedList.cpp
+++ linkedList.cpp
@@ -7,6 +7,7 @@
 
 struct Node {
 	std::string name; // Add as many data fields as needed 
+	std::string type;
 	// e.g. int age,  salary, etc.;
 	Node *next; // a pointer or link to the next node.
 }; // end of Node
@@ -17,7 +18,7 @@ class linkedList {
 		// Default, inline defined constructor that sets the beginning and end of the L.List to Null to avoid junk values.
 		linkedList(){head = NULL;tail = NULL;}
 	
-	void createNode(std::string name);
+	void createNode(std::string name, std::string type);
 
 	private:
 		Node *head, *tail;
@@ -30,21 +31,38 @@ class linkedList {
 
 int main(){
  
- linkedList students;
- students.createNode("Adam");
+ linkedList pokemon;
+ pokemon.createNode("pikachu", "electric");
  return 0; 
 }
 
 //---------------function definitions-----------------------
 // aka the "*.cpp" files 
 
-void linkedList::createNode(std::string name) {
+void linkedList::createNode(std::string name, std::string type) {
 	
-	std::cout << "creating Node..." << std::endl;
+	// Create a new node that is not connected to the linked list.
+	Node *temp = new Node; // temp->[new Node] ->
+	temp->name = name; // [name] ->
+	temp->type = type; // [name|type] ->
+	temp->next = NULL; // [name|type] -> NULL
+
+	// Case 1: Empty List := If there is only one Node in existence--the head node
+	if (head == NULL) {
+		// link/point it to the new Node
+		head = temp; // [head] -> [name|type] -> 
+		// Now, temp is the new tail
+		tail = temp; // [head] -> [tail] -> [name|type] -> NULL
+		temp = NULL; 
+		
+	} else {
+	// Case 2: Non-empty list
+		tail->next = temp; // making sure the tail points to null [tail] -> NULL
+		tail = temp; // copy info from temp into the tail [name | type] -> NULL
+
+	}
 
 }
-
-
 
 
 
