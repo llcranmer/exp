c1866e95d549f02605421da20156d6bf09831cce
llc <touldeguiacranmer@gmail.com>
2018-03-12 21:40:07 -0500

    start of linkedList data structure

---
+++ linkedList.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+struct Node {
+	std::string name; 
+	Node *next; 
+}; // end of Node struct 
+
+int main(){
+
+ std::cout << "Hello world" << std::endl;
+ Node student; 
+ std::cout << "hello student" << std::endl;
+ return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
