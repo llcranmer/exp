bee12c5f2c6deeb0e46b1b89937f8caa568c72f3
llc <touldeguiacranmer@gmail.com>
2018-03-13 03:56:17 -0500

    createNode function Added

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
 
-// Note: To compile this code use g++ -std=c++11 *linkedList.cpp -o linkedList
-// To execute use './linkedList'
+// Use the terminal to do the following:
+// to compile: $ g++ -std=c++11 *linkedList.cpp -o linkedList
+// to execute: $ ./linkedList
+
 
 struct Node {
 	std::string name; // Add as many data fields as needed 
 	// e.g. int age,  salary, etc.;
 	Node *next; // a pointer or link to the next node.
-}; // end of Node struct 
+}; // end of Node
 
 class linkedList {
 
 	public:
 		// Default, inline defined constructor that sets the beginning and end of the L.List to Null to avoid junk values.
 		linkedList(){head = NULL;tail = NULL;}
+	
+	void createNode(std::string name);
 
 	private:
 		Node *head, *tail;
 
-};
+}; // end of linkedList 
+
+//---------------function prototypes-------------------------
+// aka  "*.h" files 
+
 
 int main(){
  
+ linkedList students;
+ students.createNode("Adam");
  return 0; 
 }
 
+//---------------function definitions-----------------------
+// aka the "*.cpp" files 
 
+void linkedList::createNode(std::string name) {
+	
+	std::cout << "creating Node..." << std::endl;
 
-
+}
 
 
 
