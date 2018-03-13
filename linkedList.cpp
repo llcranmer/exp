d98850669b5f02ac68f7c99bbdb6c2f4492c7977
llc <touldeguiacranmer@gmail.com>
2018-03-13 03:41:12 -0500

    added the node struct, linkedList class, and default constructor

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+
+// Note: To compile this code use g++ -std=c++11 *linkedList.cpp -o linkedList
+// To execute use './linkedList'
+
 struct Node {
-	std::string name; 
-	Node *next; 
+	std::string name; // Add as many data fields as needed 
+	// e.g. int age,  salary, etc.;
+	Node *next; // a pointer or link to the next node.
 }; // end of Node struct 
 
-int main(){
+class linkedList {
 
- std::cout << "Hello world" << std::endl;
- Node student; 
- std::cout << "hello student" << std::endl;
+	public:
+		// Default, inline defined constructor that sets the beginning and end of the L.List to Null to avoid junk values.
+		linkedList(){head = NULL;tail = NULL;}
+
+	private:
+		Node *head, *tail;
+
+};
+
+int main(){
+ 
  return 0; 
 }
 
